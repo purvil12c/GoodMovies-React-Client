3a4da0e529eaab70fe1e4b9ca0e30ccfddf3e258
Dipen Patel <patel.dip@husky.neu.edu>
2019-03-29 02:52:53 -0400

    Modification in App.js to add Login and Register components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Login from "./components/Login";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from "./components/Register";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <div>
+                        <Route path="/" exact component={Login}/>
+                        <Route path="/register" exact component={Register}/>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
