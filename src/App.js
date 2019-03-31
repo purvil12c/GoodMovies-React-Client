a98f1a3aa0b39a02ff03cadec68c207d306201fc
Purvil Bambharolia <purvil12c@Purvils-MacBook-Pro.local>
2019-03-31 15:47:33 -0400

    app.css reference removed from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import './App.css';
 import Login from "./components/Login";
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
