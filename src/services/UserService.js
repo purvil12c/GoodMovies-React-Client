4def8af91ff2842e264de504b2d2f68c076634fd
Dipen Patel <patel.dip@husky.neu.edu>
2019-04-07 18:54:25 -0400

    Added service for logout

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,7 @@
 const user_login_url = 'https://express-goodmovies-server.herokuapp.com/users/login';
 const user_get_profile = 'https://express-goodmovies-server.herokuapp.com/users/profile';
-const user_signup_url = ' https://express-goodmovies-server.herokuapp.com/users/signup';
+const user_signup_url = 'https://express-goodmovies-server.herokuapp.com/users/signup';
+const user_logout = 'https://express-goodmovies-server.herokuapp.com/users/logout';
 
 export default class UserService {
     login = (user) =>
@@ -30,5 +31,14 @@ export default class UserService {
                 'Content-Type': 'application/json'
             }
         }).then(response => response.json())
+
+    logout = () =>
+        fetch(user_logout, {
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        })
 
 }
