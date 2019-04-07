5374f84c614109fdd2d11be2861a60c08c274c62
Dipen Patel <patel.dip@husky.neu.edu>
2019-04-07 17:08:34 -0400

    Added method to signUp a user with field such firstname, lastname, username, password and type

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,16 +2,31 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import UserService from '../services/UserService'
 
-class Register extends React.Component{
+class Register extends React.Component {
     constructor(props) {
         super(props);
         this.userService = new UserService();
         this.state = {
+            firstname: '',
+            lastname: '',
             username: '',
             password: '',
-            verifyPassword: ''
+            verifyPassword: '',
+            type: 'USER'
         }
     }
+
+    firstnameChanged = (event) => {
+        this.setState({
+                          firstname: event.target.value
+                      })
+    };
+
+    lastnameChanged = (event) => {
+        this.setState({
+                          lastname: event.target.value
+                      })
+    };
 
     usernameChanged = (event) => {
         this.setState({
@@ -31,11 +46,23 @@ class Register extends React.Component{
                       })
     };
 
+    typeChanged = (event) => {
+        console.log(event.target.value)
+        this.setState({
+                          type: event.target.value
+                      })
+    };
+
     registerUser = () => {
         var user = {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
             username: this.state.username,
-            password: this.state.password
+            password: this.state.password,
+            type: this.state.type
         };
+        //The user object which we pass in backend to create an instance
+        console.log(user)
         if (this.state.username == '' || this.state.password == '') {
             alert('Cannot leave the username or password blank')
             this.setState({
@@ -43,20 +70,22 @@ class Register extends React.Component{
                               password: '',
                               verifyPassword: ''
                           })
-        }
-        else if (this.state.password !== this.state.verifyPassword) {
+        } else if (this.state.password !== this.state.verifyPassword) {
             alert('Password does not match. Try again!');
             this.setState({
                               password: '',
                               verifyPassword: ''
                           })
         } else {
-            this.userService.register(user).then(response => {
+            this.userService.signUp(user).then(response => {
                 if (response.username != null) {
                     this.props.history.push('/profile')
-                } else if (response.username == null) {
-                    alert("This username is already taken. Try some other username.")
+                } else if (response.message) {
+                    alert("Seems like this username is already taken. Try some other username.");
                     this.setState({
+                                      firstname: '',
+                                      lastname: '',
+                                      type: 'USER',
                                       username: '',
                                       password: '',
                                       verifyPassword: ''
@@ -72,13 +101,35 @@ class Register extends React.Component{
                 <div className={"box"}>
                     <form className={"box-elements"}>
                         <h1>Sign Up</h1>
-                        <input className={"form-control my-2"} type={"text"} placeholder={"Username"}
-                        onChange={this.usernameChanged}/>
-                        <input className={"form-control my-2"} type={"password"} placeholder={"Password"}
-                        onChange={this.passwordChanged}/>
-                        <input className={"form-control my-2"} type={"password"} placeholder={"Verify Password"}
-                        onChange={this.verifyPasswordChanged}/>
-                        <button className={"red-button my-2"} onClick={this.registerUser}>
+                        <input className={"form-control my-2"} type={"text"}
+                               placeholder={"Firstname"}
+                               onChange={this.firstnameChanged}
+                               value={this.state.firstname}/>
+                        <input className={"form-control my-2"} type={"text"}
+                               placeholder={"Lastname"}
+                               onChange={this.lastnameChanged}
+                               value={this.state.lastname}/>
+                        <input className={"form-control my-2"} type={"text"}
+                               placeholder={"Username"}
+                               onChange={this.usernameChanged}
+                               value={this.state.username}/>
+                        <input className={"form-control my-2"} type={"password"}
+                               placeholder={"Password"}
+                               onChange={this.passwordChanged}
+                               value={this.state.password}/>
+                        <input className={"form-control my-2"} type={"password"}
+                               placeholder={"Verify Password"}
+                               onChange={this.verifyPasswordChanged}
+                               value={this.state.verifyPassword}/>
+
+                        <select id="role" className="form-control" onChange={this.typeChanged}
+                                value={this.state.type}>
+                            <option value="USER">User</option>
+                            <option value="CRITIC">Critic</option>
+                        </select>
+
+                        <button className={"red-button my-2"} onClick={this.registerUser}
+                                type={"button"}>
                             Sign Up
                         </button>
                         <p>
