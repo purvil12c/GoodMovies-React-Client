163b4455c3c11f9cadb9483334c5e2fab111f13b
Shivam Bhalla <shivam.bhalla10@gmail.com>
2019-04-07 17:22:05 -0400

    Create initial version of movie search layout with API integration

---
+++ src/components/MovieSearchComponent/MovieSearchComponent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {HomeNavigationBar} from "../../home/components/HomeNavigationBar";
+import MovieServiceClient from "../../services/MovieService";
+import {MovieSearchResultItem} from "./MovieSearchResultItem";
+
+export default class MovieSearchComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResults: []
+        }
+    }
+
+    componentDidMount() {
+        // Load popular movies and in theatre movies from API here and use them below
+        MovieServiceClient.instance.searchMovieForQuery(this.props.match.params.query).then(response => {
+            console.log(response.results);
+            this.setState({
+                searchResults: response.results
+            })
+
+        })
+    }
+
+    renderSearchResults() {
+        if(this.state.searchResults) {
+            let items = this.state.searchResults
+                .map(function(result) {
+                    return <MovieSearchResultItem searchResult={result}/>;
+                });
+            return (items);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <HomeNavigationBar loggedIn = {true}/>
+                <h6 className={'m-4 col-12'}> Search results for "{this.props.match.params.query}" </h6>
+                <div className={"col-12 mt-4"}>
+                    <ul>
+                        {this.renderSearchResults()}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/components/MovieSearchComponent/MovieSearchResultItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import * as constants from '../../services/Constants'
+export const MovieSearchResultItem = ({searchResult}) => {
+    return (
+        <Link className='col-2' to={`/movie/${searchResult.id}`}>
+        <li className={'card'}>
+            <div className="row no-gutters">
+                <div className="col-auto">
+                    <img src={constants.TMDB_IMAGE_BASE_URL + '/w200' + searchResult.poster_path} className="img-fluid m-2" alt=""/>
+                </div>
+                <div className="col">
+                    <div className="card-block">
+                        <h6 className="card-title m-4">{searchResult.title}</h6>
+                    </div>
+                </div>
+            </div>
+        </li>
+        </Link>
+)
+}
