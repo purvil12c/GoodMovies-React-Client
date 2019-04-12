fe52ba58281b32603c8aaf7e6ca0d4363b5737ba
Dipen <patel.dip@husky.neu.edu>
2019-04-12 17:10:47 -0400

    Added icon for removing a movie from watchlist

--- src/components/UserWatchList.js
+++ src/components/UserWatchList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 class UserWatchList extends React.Component {
     constructor(props) {
@@ -17,9 +18,12 @@ class UserWatchList extends React.Component {
                         this.state.watchlist.map(movie => {
                             return (
                                 <li className="list-group-item">
-                                    <Link to={'/movie/' + movie.movieId}>
-                                        {movie.movieName}
-                                    </Link>
+                                    <div>
+                                        <Link to={'/movie/' + movie.movieId}>
+                                            {movie.movieName}
+                                        </Link>
+                                        <i className="fa fa-times float-right"></i>
+                                    </div>
                                 </li>
                             )
                         })
