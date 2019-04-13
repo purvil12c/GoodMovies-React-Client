67cfe2cabc89a85525abeb6f1947b553d0fbfa58
Dipen <patel.dip@husky.neu.edu>
2019-04-13 00:38:58 -0400

    /profile/:id is the new Link

--- src/home/containers/HomeContainer.js
+++ src/home/containers/HomeContainer.js
@@ -19,8 +19,8 @@ export default class HomeContainer extends React.Component {
 
         this.userService.getProfile().then(
             response => this.setState({
-                userProfile: response
-            })
+                                          userProfile: response
+                                      })
         )
 
         this.searchTextUpdated = this.searchTextUpdated.bind(this);
@@ -31,21 +31,21 @@ export default class HomeContainer extends React.Component {
     componentDidMount() {
         MovieServiceClient.instance.getPopularMovies().then(response => {
             this.setState({
-                popularMovies: response.results
-            })
+                              popularMovies: response.results
+                          })
         })
 
         MovieServiceClient.instance.getNowPlayingMovies().then(response => {
             this.setState({
-                nowPlayingMovies: response.results
-            })
+                              nowPlayingMovies: response.results
+                          })
         })
     }
 
     searchTextUpdated(event) {
         this.setState({
-            searchQuery: event.target.value
-        })
+                          searchQuery: event.target.value
+                      })
     }
 
     searchButtonClicked() {
@@ -70,16 +70,19 @@ export default class HomeContainer extends React.Component {
                     this.state.userProfile.username !== undefined &&
                     <HomeNavigationBar loggedIn={true}
                                        username={this.state.userProfile.username}
+                                       userId={this.state.userProfile._id}
                                        logout={this.logout}/>
                 }
 
                 <div className={"row"}>
                     <div className="md-form m-4 col-9">
-                        <input className="form-control" type="text" onChange={this.searchTextUpdated}
+                        <input className="form-control" type="text"
+                               onChange={this.searchTextUpdated}
                                placeholder="Search" value={this.state.searchQuery}
                                aria-label="Search"></input>
                     </div>
-                    <button type='btn' className="text-white btn bg-primary" onClick={this.searchButtonClicked}>
+                    <button type='btn' className="text-white btn bg-primary"
+                            onClick={this.searchButtonClicked}>
                         GO
                     </button>
                     <div className="btn-group btn-group-toggle m-4" data-toggle="buttons">
