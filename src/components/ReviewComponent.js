fa3379dd632f50e671bcaf14561881b273e73d17
Dipen <patel.dip@husky.neu.edu>
2019-04-13 23:28:40 -0400

    Displayed a review properly in card:

--- src/components/ReviewComponent.js
+++ src/components/ReviewComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ReviewComponent = ({review}) =>
+const ReviewComponent = ({review, deleteReview}) =>
     review !== null &&
     <div className="card my-2">
         <div className="card-header">
-            {review.movieId} : Movie title here
-            <i className="fa fa-times float-right"></i>
+            {review.movieName}
+            <i className="fa fa-times float-right"
+               onClick={() => deleteReview(review._id)}
+            ></i>
         </div>
         <div className="card-body">
             <h5 className="card-title">{review.title}</h5>
