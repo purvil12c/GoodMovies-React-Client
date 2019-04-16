be9d3e1c34d28b960053918ddabc5056ca25bb8c
Purvil Bambharolia <purvil12c@Purvils-MacBook-Pro.local>
2019-04-16 15:27:00 -0400

    intro screen added

---
+++ src/components/IntroPageComponent/IntroPageComponent.js
@@ -0,0 +1,93 @@
+import React from "react";
+import './IntroPageComponent.css'
+import posed from 'react-pose';
+import {Link} from 'react-router-dom'
+
+const AnimatedDivText1 = posed.div({
+  hidden: { opacity: 0 },
+  visible: { opacity: 1}
+});
+
+const AnimatedDivText2 = posed.div({
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0}
+});
+
+const AnimatedDivVisitWebsite= posed.div({
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0}
+});
+
+const AnimatedDivFooter= posed.div({
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0}
+});
+
+
+class IntroPageComponent extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isVisibleText1: false,
+      isVisibleText2: false,
+      isVisibleButton: false,
+      isVisibleFooter: false
+    }
+  }
+
+  componentDidMount() {
+      setTimeout(() => {
+        this.setState({ isVisibleText1: !this.state.isVisibleText1 });
+      }, 1000);
+
+      setTimeout(() => {
+        this.setState({ isVisibleText2: !this.state.isVisibleText2 });
+      }, 2000);
+
+      setTimeout(() => {
+        this.setState({ isVisibleButton: !this.state.isVisibleButton });
+      }, 3000);
+
+      setTimeout(() => {
+        this.setState({ isVisibleFooter: !this.state.isVisibleFooter });
+      }, 3000);
+  }
+
+  render(){
+    return(
+      <div className="center">
+
+        <AnimatedDivText1 pose={this.state.isVisibleText1 ? 'visible' : 'hidden'}>
+          <h1 className="white-title">Welcome to GoodMovies</h1>
+        </AnimatedDivText1>
+
+        <AnimatedDivText2 pose={this.state.isVisibleText2 ? 'visible' : 'hidden'}>
+          <h4 className="white-title">Discover your next favorite.</h4>
+        </AnimatedDivText2>
+        <br/>
+
+        <AnimatedDivVisitWebsite pose={this.state.isVisibleText2 ? 'visible' : 'hidden'}>
+          <Link to="/home" className="btn btn-outline-success">Browse Movies</Link>
+        </AnimatedDivVisitWebsite>
+
+        <AnimatedDivFooter pose={this.state.isVisibleFooter ? 'visible' : 'hidden'} className="footer">
+          <div className="row">
+            <div className="col-xs-12 col-md-4 white-title">
+              <h4>Purvil Bambharolia</h4>
+            </div>
+            <div className="col-xs-12 col-md-4 white-title">
+              <h4>Dipen Patel</h4>
+            </div>
+            <div className="col-xs-12 col-md-4 white-title">
+              <h4>Shivam Bhalla</h4>
+            </div>
+          </div>
+        </AnimatedDivFooter>
+
+      </div>
+    )
+  }
+}
+
+export default IntroPageComponent;
