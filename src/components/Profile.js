e70ad4934bcff5a36f2ebef7b05796d34d2f2581
Dipen <patel.dip@husky.neu.edu>
2019-04-17 13:36:41 -0400

    Follow unfollow button fixed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -138,6 +138,12 @@ class Profile extends React.Component {
                                      }))
 
     render() {
+        if (this.state.user != undefined &&
+            this.state.loggedInUser.username !== undefined &&
+            this.state.loggedInUser._id !== this.state.user._id) {
+            var followers = [];
+            this.state.followers.map(user => followers.push(user.username))
+        }
         return (
             <AnimatedDiv pose={this.state.isVisible ? 'visible' : 'hidden'}>
                 <div>
@@ -164,11 +170,12 @@ class Profile extends React.Component {
                                         }
                                     </div>
                                     <div className={"col-sm-12 col-md-6 col-lg-8"}>
-                                        {
-                                            this.state.user != undefined &&
-                                            this.state.loggedInUser.username !== undefined &&
-                                            this.state.loggedInUser._id !== this.state.user._id &&
-                                            <div className={"float-right"}>
+                                        <div className={"float-right"}>
+                                            {
+                                                this.state.user != undefined &&
+                                                this.state.loggedInUser.username !== undefined &&
+                                                this.state.loggedInUser._id !== this.state.user._id &&
+                                                followers.includes(this.state.loggedInUser.username) == false &&
                                                 <button className={"btn btn-primary my-2"}
                                                         type={"button"}
                                                         onClick={() => this.followUser(
@@ -176,6 +183,12 @@ class Profile extends React.Component {
                                                             this.state.id)}>
                                                     Follow
                                                 </button>
+                                            }
+                                            {
+                                                this.state.user != undefined &&
+                                                this.state.loggedInUser.username !== undefined &&
+                                                this.state.loggedInUser._id !== this.state.user._id &&
+                                                followers.includes(this.state.loggedInUser.username) &&
                                                 <button className={"btn btn-primary my-2"}
                                                         type={"button"}
                                                         onClick={() => this.unfollowUser(
@@ -183,8 +196,8 @@ class Profile extends React.Component {
                                                             this.state.id)}>
                                                     Unfollow
                                                 </button>
-                                            </div>
-                                        }
+                                            }
+                                        </div>
                                     </div>
                                 </div>
                             </div>
