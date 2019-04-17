2ed807fbc2be7e310a1bab03b6b4095b7338aef2
Dipen <patel.dip@husky.neu.edu>
2019-04-17 17:13:35 -0400

    Added email field

--- src/components/UserInformation.js
+++ src/components/UserInformation.js
@@ -7,7 +7,8 @@ class UserInformation extends React.Component {
         this.userService = new UserService();
         this.state = ({
             firstname: this.props.user.firstname,
-            lastname: this.props.user.lastname
+            lastname: this.props.user.lastname,
+            email: this.props.user.email
         })
 
     }
@@ -24,12 +25,19 @@ class UserInformation extends React.Component {
                       })
     };
 
+    emailChanged = (event) => {
+        this.setState({
+                          email: event.target.value
+                      })
+    };
+
     updateUser = () => {
         var userId = this.props.user._id;
         var user = {
             _id: userId,
             firstname: this.state.firstname,
-            lastname: this.state.lastname
+            lastname: this.state.lastname,
+            email: this.state.email
         };
         this.props.updateUser(user, userId);
         alert('User Updated');
@@ -63,7 +71,9 @@ class UserInformation extends React.Component {
                         Email</label>
                     <div className={"col sm-10"}>
                         <input className="form-control" id="email"
-                               placeholder="xyz@goodmovies.com"/>
+                               placeholder="Email"
+                               value={this.state.email}
+                               onChange={this.emailChanged}/>
                     </div>
                 </div>
                 <div>
