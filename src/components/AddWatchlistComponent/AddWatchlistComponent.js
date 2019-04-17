3498068ffd02ca344b374c8dd49567dba7ddc778
Purvil Bambharolia <purvil12c@Purvils-MacBook-Pro.local>
2019-04-17 17:41:52 -0400

    redesigned reviews in profile

--- src/components/AddWatchlistComponent/AddWatchlistComponent.js
+++ src/components/AddWatchlistComponent/AddWatchlistComponent.js
@@ -35,7 +35,7 @@ export default class AddWatchlistComponent extends React.Component {
     }
 
     addToWatchlist() {
-        MovieServiceClient.instance.addMovieToWatchlist(this.props.userId, this.props.movie.id, this.props.movie.title, constants.TMDB_IMAGE_BASE_URL + '/w92' + this.props.movie.poster_path).then(response => {
+        MovieServiceClient.instance.addMovieToWatchlist(this.props.userId, this.props.movie.id, this.props.movie.title, constants.TMDB_IMAGE_BASE_URL + '/w185' + this.props.movie.poster_path).then(response => {
 
             this.userService.findUserById(this.props.userId).then(response => {
 
